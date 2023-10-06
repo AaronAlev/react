f2adf92f1da7ae3ff14b59bd673c711e84d963f4
Aaron Alev <aaronalev@gmail.com>
2023-10-06 13:27:54 +0300

    css ylesanne 1

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,13 +1,14 @@
+import './ExpenseDate.css'
 function ExpenseDate(props) {
     const day = props.date.toLocaleString('en-US', {day: '2-digit'})
     const month = props.date.toLocaleString('en-US', {month: 'long'})
     const year = props.date.getFullYear()
 
     return (
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
+        <div class='expense-date'>
+            <div class='expense-date__month'>{month}</div>
+            <div class='expense-date__day'>{day}</div>
+            <div class='expense-date__year'>{year}</div>
         </div>
     )
 }
